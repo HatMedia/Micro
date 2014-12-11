c915b01abf498e7cc155b0d1e6a5eb3ca934ee7a
Matti van de Weem <mattiweem@hotmail.com>
2014-12-11 17:53:27 +0100

    More minor tweaks

--- App/Models/Users.php
+++ App/Models/Users.php
@@ -43,21 +43,18 @@ class Users{
 
 	public function insertUser($username, $password, $role){
 
-		try{
-			$username = $this->app->escape($username);
-			$password = $this->app->escape($password);
-			$role = $this->app->escape($role);
-			$password =  $this->app['security.encoder.digest']->encodePassword($password, '');
-			$sql = 'INSERT INTO users (username,password,roles) VALUES (?,?,?)';
-			$stmt = $this->app['db']->prepare($sql);
-			$stmt->bindValue(1, $username);
-			$stmt->bindValue(2, $password);
-			$stmt->bindValue(3, $role);
+		$username = $this->app->escape($username);
+		$password = $this->app->escape($password);
+		$role = $this->app->escape($role);
+		$password =  $this->app['security.encoder.digest']->encodePassword($password, '');
+		$sql = 'INSERT INTO users (username,password,roles) VALUES (?,?,?)';
+		$stmt = $this->app['db']->prepare($sql);
+		$stmt->bindValue(1, $username);
+		$stmt->bindValue(2, $password);
+		$stmt->bindValue(3, $role);
 
-			$stmt->execute();
-		} catch (Exception $e) {
-			return false;
-		}
+		$stmt->execute();
+
 		return true;
 	}
 
